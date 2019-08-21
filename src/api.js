4bae8f6041b0af66a56df5ba9378083f6c10b762
Tyler Leung <binglux@gmail.com>
2019-08-22 02:24:15 +0800

    fix(sound): add trigger to resume audio context

--- src/api.js
+++ src/api.js
@@ -2,14 +2,6 @@ export const fetchUserLocation = () => {
   return fetchWithData("https://api.ipdata.co?api-key=fb9dfde35d54ee96cbb2abfa8a573182071cf91c14bc89dc7248a6c5");
 };
 
-export const fetchSoundList = () => {
-  return fetchWithData();
-};
-
-export const fetchSound = () => {
-  return fetchWithData();
-};
-
 const fetchWithData = (
   url = '',
   method = 'GET',
--- src/components/Music.js
+++ src/components/Music.js
@@ -3,16 +3,59 @@ import VolumeUpIcon from '@material-ui/icons/VolumeUp';
 import './Music.css';
 
 const Music = () => {
-  const [isPlaying, setIsPlaying] = useState(true);
+  const soundList = [0, 1, 2, 3, 4].map(index => {
+    return `https://uniqlock.s3-ap-northeast-1.amazonaws.com/uniqlo_extra/uniqlock2/sound/sound${index}.mp3`;
+  });
 
+  const [isPlaying, setIsPlaying] = useState(true);
+  const [soundURL, setSoundURL] = useState(soundList[0]);
+
+
+  const context = new AudioContext();
   const toggleMusic = () => {
-    setIsPlaying(!isPlaying);
+    // setIsPlaying(!isPlaying);
+    // context.suspend();
+    // console.log("hello");
   };
 
   useEffect(() => {
-    // const audio = new Audio(soundFiles);
-    // audio.play();
-  }, []);
+
+    document.getElementById('root').addEventListener('click', () => {
+      context.resume().then(() => {
+        console.log("playback.");
+      });
+    });
+
+    const play = audioBuffer => {
+      const source = context.createBufferSource();
+      source.buffer = audioBuffer;
+      source.connect(context.destination);
+      source.start();
+      source.onended = getNextSound;
+    };
+
+    const fetchSound = () => {
+      if (soundURL === "") {
+        setSoundURL(soundList[0]);
+      }
+      window.fetch(soundURL)
+        .then(response => response.arrayBuffer())
+        .then(arrayBuffer => context.decodeAudioData(arrayBuffer))
+        .then(audioBuffer => {
+          play(audioBuffer);
+        });
+    };
+
+    const getNextSound = () => {
+      const currentSoundIndex = soundList.indexOf(soundURL);
+      if (currentSoundIndex + 1 === soundList.length) {
+        setSoundURL(soundList[0]);
+      } else {
+        setSoundURL(soundList[currentSoundIndex + 1]);
+      }
+    };
+    fetchSound();
+  }, [soundURL]);
 
   return (
     <div>
